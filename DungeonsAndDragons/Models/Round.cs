7bdc72df8f9283ecf1849a4f9a3b64425f4d00a1
antonborisov571 <antonborisov571@gmail.com>
2024-01-09 14:55:13 +0300

    Добавил модель результата боя и результата раунда

---
+++ DungeonsAndDragons/Models/Round.cs
@@ -0,0 +1,13 @@
+﻿namespace Models;
+
+public class Round
+{
+	public string Attacker { get; set; }
+	public string Defender { get; set; }
+	public int AttackModifier { get; set; }
+	public int[] DiceRoll { get; set; }
+	public int[] Damage { get; set; }
+	public string[] Strike { get; set; }
+	public int[] DefenderHp { get; set; }
+
+}
