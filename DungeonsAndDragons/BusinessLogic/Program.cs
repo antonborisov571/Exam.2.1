85176f8c67f568d8f482d883de57c6a2a5b93ed8
antonborisov571 <antonborisov571@gmail.com>
2024-01-09 14:56:03 +0300

    Добавил фронтенд на страницу

--- DungeonsAndDragons/BusinessLogic/Program.cs
+++ DungeonsAndDragons/BusinessLogic/Program.cs
@@ -1,4 +1,5 @@
 using BusinessLogic.DatabaseContext;
+using BusinessLogic.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IFight, Fight>();
 
 var app = builder.Build();
 
--- DungeonsAndDragons/UI/Pages/Game.cshtml.cs
+++ DungeonsAndDragons/UI/Pages/Game.cshtml.cs
@@ -8,12 +8,14 @@ namespace UI.Pages;
 public class GameModel : PageModel
 {
     private readonly string GetMonsterUrl = "https://localhost:7180/Monsters/getrandom";
-    private readonly string BusinessLogicUrl = "https://localhost:7180/monsters/getrandom";
+    private readonly string BusinessLogicUrl = "https://localhost:7180/Fights/getlogfight";
 
-    public Monster Monster { get; set; }
+    public Monster? Monster { get; set; }
 
     [BindProperty]
     public Player Player { get; set; } = new();
+
+    public Result? Result { get; set; }
 
     public void OnGet()
     {
@@ -26,6 +28,7 @@ public class GameModel : PageModel
 		var monster = await client.GetFromJsonAsync<Monster>(GetMonsterUrl);
 		Monster = monster!;
         var resultFight = await client.PostAsJsonAsync(BusinessLogicUrl, new MonsterAndPlayer(Monster, Player));
+		Result = await resultFight.Content.ReadFromJsonAsync<Result>();
 	}
 
 }
